8a6c3abe74ae8b1aa90878ecf1e906f97620ab70
Jim Kettunen <jim@Jims-MacBook-Air.local>
2019-04-28 21:39:27 +0900

    part 3 done

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'jimk12',
+		'title': 'Blog Post 1',
+		'content': 'This is the first ever blog post.',
+		'datePosted': 'April 28, 2019'
+	},
+	{
+		'author': 'danielleket17',
+		'title': 'Blog Post 2',
+		'content': 'This post is from danielleket17.',
+		'datePosted': 'April 29, 2019'
+	}
+]
+
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
