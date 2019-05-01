9fcc74b8ae96e0f316ec0ff1757ca87c3cc29c10
theBionicFox <jim@bionicfox.io>
2019-05-01 10:35:56 +0900

    Moved main css to bionic directory rather than building off of the blog app.

--- web/settings.py
+++ web/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bionic.apps.BionicConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
