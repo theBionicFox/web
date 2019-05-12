e41f8b9e1e650cf96befbdc82d80d98681440dce
theBionicFox <jim@bionicfox.io>
2019-05-12 19:17:25 +0900

    user profile pictures and profile editing complete

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def home(request):
@@ -20,4 +20,23 @@ def register(request):
 
 @login_required
 def profile(request):
-		return render(request, 'users/profile.html')
+	if request.method == "POST":
+		userForm = UserUpdateForm(request.POST, instance=request.user)
+		profileForm = ProfileUpdateForm(request.POST,
+										request.FILES,
+										instance=request.user.profile)
+		if userForm.is_valid() and profileForm.is_valid():
+			userForm.save()
+			profileForm.save()
+			messages.success(request, f'Your Account has been updated')
+			return redirect('users-profile')
+	else:
+		userForm = UserUpdateForm(instance=request.user)
+		profileForm = ProfileUpdateForm(instance=request.user.profile)		
+
+	context = {
+		'userForm': userForm,
+		'profileForm': profileForm
+	}
+
+	return render(request, 'users/profile.html', context)
