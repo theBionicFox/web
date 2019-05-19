e20b7828b0716d489154817bcc5a258de921a6e2
theBionicFox <jim@bionicfox.io>
2019-05-19 22:14:47 +0900

    blog frame completed

--- web/settings.py
+++ web/settings.py
@@ -129,5 +129,11 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'bionic-home'
-
 LOGIN_URL = 'users-login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'jim@bionicfox.io'
+EMAIL_HOST_PASSWORD = 'gbybagebbwiotdlb'
